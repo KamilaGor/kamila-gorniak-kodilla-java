9eaa74b1ceac326af569c429575e0d22652146a3
KamilaGor <kamilagorniak89@gmail.com>
2019-03-05 14:04:52 +0100

    Zadanie: Test klasy wybierającej parzyste liczby z kolekcji plus rozbudowana klasa SimpleUser (z forumName i realName)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,31 +1,13 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.collection.OddNumbersExterminator;
 import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 
+import java.util.ArrayList;
+
 public class TestingMain {
     public static void main (String[] args) {
-        // dlatego, że mel
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = ((SimpleUser) simpleUser).getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test ok");
-        } else {
-            System.out.println("Error!");
-        }
-        System.out.println();
-        System.out.println("Test - pierwszy test jednostkowy: ");
-        /* dlatego, że metoda statyczna, odwołuję się bezpośrednio przez nazwę klasy
-        (zmienne statyczne klasy dotyczą wszystkich jej obiektów) */
-        int result1 = Calculator.add(3,8);
-        int result2 = Calculator.subtract(3,8);
-        if (result1==11 && result2==5) {
-            System.out.println("pierwszy test jednostkowy ok");
-        } else {
-            System.out.println("pierwszy test jednostkowy - error !");
-        }
 
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+
+        for (Integer theNumber: numbers) {
+            if(theNumber%2==0){
+                numbers2.add(theNumber);
+            }
+        }
+        return numbers2;
+    }
+}
+
