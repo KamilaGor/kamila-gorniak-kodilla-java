a5f02941c20b03285e72308b6e6905bab4aa9185
KamilaGor <kamilagorniak89@gmail.com>
2019-03-26 21:56:44 +0100

    7.3 zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,23 +1,25 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
-
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String [] args) {
-        BookDirectory theBookDirectory = new BookDirectory();
+        Forum theForum = new Forum();
 
-        Map<String, Book> theResultMapofBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toMap(Book::getSignature, book -> book));
+        Map<Integer, ForumUser> theResultMapOfUsers = theForum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex()=='M')
+                .filter(forumUser -> forumUser.getBirthDate().getYear()<=1999)
+                .filter(forumUser -> forumUser.getPostsNumber()>=1)
+                .collect(Collectors.toMap(ForumUser::getUserNumber, forumUser -> forumUser));
 
-        System.out.println("# elements: " + theResultMapofBooks.size());
-        theResultMapofBooks.entrySet().stream()
+        System.out.println("Result Map of Users: " );
+        theResultMapOfUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
+
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,29 @@
+package com.kodilla.stream.forumuser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+
+    private final List<ForumUser> forumUserList = new ArrayList<>();
+
+    public Forum() {
+        forumUserList.add(new ForumUser(001, "UserName1", 'M', 1991,
+                01, 01, 91));
+        forumUserList.add(new ForumUser(002, "UserName2", 'F', 2002,
+                02, 02, 2));
+        forumUserList.add(new ForumUser(003, "UserName3", 'M', 1993,
+                03, 03, 0));
+        forumUserList.add(new ForumUser(004, "UserName4", 'F', 2004,
+                04, 04, 4));
+        forumUserList.add(new ForumUser(005, "UserName5", 'M', 1995,
+                05, 05, 5));
+        forumUserList.add(new ForumUser(006, "UserName6", 'F', 1996,
+                06, 06, 0));
+        forumUserList.add(new ForumUser(007, "UserName7", 'M', 2007,
+                07, 07, 7));
+    }
+    public List<ForumUser> getUserList() {
+        return new ArrayList<> (forumUserList);
+    }
+}
