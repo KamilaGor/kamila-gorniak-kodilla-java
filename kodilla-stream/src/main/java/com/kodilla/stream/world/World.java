f809f7fc15b21f809f62df5badf2f7d77e18899f
KamilaGor <kamilagorniak89@gmail.com>
2019-03-28 23:28:43 +0100

    7.4 zadanie - poznanie flatMap i reduce- ilosc ludzi na swiecie

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+    private final List<Continent> continentsList = new ArrayList<>();
+
+    public  void addContinent(Continent continent) {
+        continentsList.add(continent);
+    }
+    public List<Continent> getContinentsList() {
+        return continentsList;
+    }
+    public BigDecimal getPeopleQuantity() {
+        return continentsList.stream()
+                .flatMap(world -> world.getCountries().stream())
+                .map(Country::getPeopleQuantity)
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+    }
+}
