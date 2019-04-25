2289f2ce6bf9b7c18b5dacaeeb8b78686babb862
KamilaGor <kamilagorniak89@gmail.com>
2019-04-25 17:37:16 +0200

    Dział 8 - wyjątki Exception

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,7 +8,8 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+
+    public void readFile() throws FileReaderException {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
@@ -19,11 +20,25 @@ public class FileReader {
 
         } catch (IOException e) {
 
-            System.out.println("Oh no! STH went wrong! Error:" + e);
+            throw new FileReaderException();
 
         } finally {
 
             System.out.println(" I am gonna be here... always!");
         }
     }
+//    public void readFile() FileReaderException {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource("file/names.txt").getFile());
+//
+//
+//        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+//
+//            fileLines.forEach(System.out::println);
+//        } catch (IOException e) {
+//            throw new FileReaderException();
+//        } finally {
+//            System.out.println("I am gonna be here... always");
+//        }
+//    }
 }
