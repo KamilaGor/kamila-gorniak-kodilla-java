b21f5420d9e15b3c1074831fa6e8d1404cc36527
KamilaGor <kamilagorniak89@gmail.com>
2019-06-24 11:21:06 +0200

    zadanie 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreRunner.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreRunner {
+	public static void main (String[] args) {
+		String namesOfMovies = MovieStore.getMovies().entrySet().stream()
+				.flatMap(movieStore -> movieStore.getValue().stream())
+				.collect(Collectors.joining("! ", "", "!" ));
+		System.out.println(namesOfMovies);
+	}
+}
