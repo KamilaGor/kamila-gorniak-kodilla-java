57c7075262e76e5e445e7bc65d1e928d34e5f3f8
KamilaGor <kamilagorniak89@gmail.com>
2019-07-16 21:41:28 +0200

    Moduł 14.2 - przykłady zadań: @Component, @Service, @Controller, @Repository, zasięgi Beana, metoda getBean(Class class)/ getBean(String name)

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/LibraryDbController.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.library;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LibraryDbController {
+	public void saveData() { //zapisywanie stanu księgozbioru
+		System.out.println("Saving data to the database");
+	}
+	public void loadData() { //odczytywanie stanu księgozbioru
+		System.out.println("Loading data from the database");
+	}
+}
