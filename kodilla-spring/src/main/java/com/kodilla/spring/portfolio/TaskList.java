6dd206b02ad6b0dd8ab4c98d9cac082c4bbcde4e
KamilaGor <kamilagorniak89@gmail.com>
2019-07-24 16:38:42 +0200

    14.3 plus portfolio

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,17 @@
+//package com.kodilla.spring.portfolio;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public final class TaskList {
+//
+//	private List<String> tasks = new ArrayList<>();
+//
+//	public TaskList() {
+//	}
+//	//Utwórz również klasę TaskList w tym samym pakiecie - powinna ona zawierać właściwość List<String> tasks.
+//	//Jej zawartość powinna być inicjowana w konstruktorze przy pomocy polecenia tasks = new ArrayList<>();
+//}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,30 @@
+//package com.kodilla.spring.portfolio;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.ArrayList;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class BoardTestSuite {
+//	//Utwórz też test testTaskAdd(), który doda do każdej z list po jednym zadaniu (wcześniej uzyskując dostęp do Beana klasy Board).
+//	//Test powinien sprawdzać czy dodane zadania da się odczytać.
+//	@Test
+//	public void testTaskAdd() {
+//		//Given
+//		ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+//		TaskList taskList = context.getBean(TaskList.class);
+//		//When
+//
+//		//Then
+//		System.out.println(taskList);
+//
+//	}
+//}
+//
+//
