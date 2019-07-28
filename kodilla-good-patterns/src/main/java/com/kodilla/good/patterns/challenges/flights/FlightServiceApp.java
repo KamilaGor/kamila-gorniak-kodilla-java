6aa8f36fb7aeaf2240db422143bf7306428ac952
KamilaGor <kamilagorniak89@gmail.com>
2019-07-28 17:27:51 +0200

    14.4 Spring Web

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightServiceApp.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightServiceApp.java
@@ -1,21 +1,21 @@
-package com.kodilla.good.patterns.challenges.flights;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
-public class FlightServiceApp {
-
-	public static void main(String[] args) {
-		System.out.println("Wyloty z Gdańska:");
-		FlightConnection1 departures = new FlightConnection1();
-		departures.getSet().stream()
-				.filter(flight -> flight.getAirportDepartures().equals("Gdańsk"))
-				.forEach(System.out::println);
-		System.out.println("Lądowanie w Poznaniu: ");
-		FlightConnection1 arrivals = new FlightConnection1();
-		arrivals.getSet().stream()
-				.filter(flight -> flight.getAirportArrivals().equals("Poznań"))
-				.forEach(System.out::println);
-		System.out.println("Międzylądowanie:");
-	}
-}
+//package com.kodilla.good.patterns.challenges.flights;
+//
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//public class FlightServiceApp {
+//
+//	public static void main(String[] args) {
+//		System.out.println("Wyloty z Gdańska:");
+//		FlightConnection1 departures = new FlightConnection1();
+//		departures.getSet().stream()
+//				.filter(flight -> flight.getAirportDepartures().equals("Gdańsk"))
+//				.forEach(System.out::println);
+//		System.out.println("Lądowanie w Poznaniu: ");
+//		FlightConnection1 arrivals = new FlightConnection1();
+//		arrivals.getSet().stream()
+//				.filter(flight -> flight.getAirportArrivals().equals("Poznań"))
+//				.forEach(System.out::println);
+//		System.out.println("Międzylądowanie:");
+//	}
+//}
