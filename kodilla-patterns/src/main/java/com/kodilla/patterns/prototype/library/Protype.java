019846ce640780c6f1e79d52b87852ccbc67aad8
KamilaGor <kamilagorniak89@gmail.com>
2019-08-04 11:59:11 +0200

    15.2 zadanie

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Protype.java
@@ -0,0 +1,9 @@
+package com.kodilla.patterns.prototype.library;
+
+public class Protype<Library> implements Cloneable {
+	@Override
+	public Library clone()throws CloneNotSupportedException {
+
+		return (Library)super.clone();
+	}
+}
