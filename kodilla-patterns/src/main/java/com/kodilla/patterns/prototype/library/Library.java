9060f536d71a67ee51074f81a93f0375031ce019
KamilaGor <kamilagorniak89@gmail.com>
2019-08-05 11:09:36 +0200

    przykład 15.3

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -1,43 +1,43 @@
-package com.kodilla.patterns.prototype.library;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public final class Library extends Protype {
-	String name;
-	Set<Book> books = new HashSet<>();
-
-	public Library(final String name) {
-		this.name = name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public Set<Book> getBooks() {
-		return books;
-	}
-
-	@Override
-	public String toString() {
-		return "Library{" +
-				"name='" + name + '\'' +
-				", books=" + books +
-				'}';
-	}
-
-	public Library shallowCopy() throws CloneNotSupportedException {
-		return (Library)super.clone();
-	}
-	public Library deepCopy() throws CloneNotSupportedException {
-		Library clonedList = (Library)super.clone();
-		clonedList.books = new HashSet<>();
-		clonedList.getBooks().add()
-
-	}
-}
+//package com.kodilla.patterns.prototype.library;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public final class Library extends Protype {
+//	String name;
+//	Set<Book> books = new HashSet<>();
+//
+//	public Library(final String name) {
+//		this.name = name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public Set<Book> getBooks() {
+//		return books;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Library{" +
+//				"name='" + name + '\'' +
+//				", books=" + books +
+//				'}';
+//	}
+//
+//	public Library shallowCopy() throws CloneNotSupportedException {
+//		return (Library)super.clone();
+//	}
+//	public Library deepCopy() throws CloneNotSupportedException {
+//		Library clonedList = (Library)super.clone();
+//		clonedList.books = new HashSet<>();
+//		clonedList.getBooks().add()
+//
+//	}
+//}
--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,37 +1,37 @@
-package com.kodilla.patterns.prototype.library;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
-public class LibraryTestSuite {
-	@Test
-	public void testGetBooks() {
-		//Given
-		Library list = new Library("LLL1");
-		list.books.add(new Book("one", "AA", LocalDate.of(2001, 01, 01)));
-		list.books.add(new Book("two", "BB", LocalDate.of(2002, 02, 02)));
-		list.books.add(new Book("three", "CC", LocalDate.of(2003, 03, 03)));
-		list.books.add(new Book("four", "DD", LocalDate.of(2004, 04, 04)));
-		list.books.add(new Book("five", "EE", LocalDate.of(2005, 05, 05)));
-
-		//making a shallow clone of object library
-		Library clonedList = null;
-		try {
-			clonedList = list.shallowCopy();
-			clonedList.setName("LLL2");
-		} catch (CloneNotSupportedException e) {
-			System.out.println(e);
-		}
-		System.out.println(list);
-		System.out.println(clonedList);
-
-		//When
-		//Then
-		Assert.assertEquals(5, list.books.size());
-		Assert.assertEquals(5, clonedList.books.size());
-	}
-}
+//package com.kodilla.patterns.prototype.library;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import java.time.LocalDate;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public class LibraryTestSuite {
+//	@Test
+//	public void testGetBooks() {
+//		//Given
+//		Library list = new Library("LLL1");
+//		list.books.add(new Book("one", "AA", LocalDate.of(2001, 01, 01)));
+//		list.books.add(new Book("two", "BB", LocalDate.of(2002, 02, 02)));
+//		list.books.add(new Book("three", "CC", LocalDate.of(2003, 03, 03)));
+//		list.books.add(new Book("four", "DD", LocalDate.of(2004, 04, 04)));
+//		list.books.add(new Book("five", "EE", LocalDate.of(2005, 05, 05)));
+//
+//		//making a shallow clone of object library
+//		Library clonedList = null;
+//		try {
+//			clonedList = list.shallowCopy();
+//			clonedList.setName("LLL2");
+//		} catch (CloneNotSupportedException e) {
+//			System.out.println(e);
+//		}
+//		System.out.println(list);
+//		System.out.println(clonedList);
+//
+//		//When
+//		//Then
+//		Assert.assertEquals(5, list.books.size());
+//		Assert.assertEquals(5, clonedList.books.size());
+//	}
+//}
