60ce0bb6f1735135b76f362feb3d688483c36245
KamilaGor <kamilagorniak89@gmail.com>
2019-08-17 22:19:25 +0200

    przykład 17.2

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -29,11 +31,28 @@ public class TaskDaoTestSuite {
 
 		//Then
 		int id = task.getId(); // z kontrolera repozytorium pobieramy obiekt o wskazanym identyfikatorze
-		Optional&lt;Task&gt; readTask = taskDao.findById(id); //metoda zwraca Optional&lt;Task&gt;
+		Optional<Task> readTask = taskDao.findById(id); //metoda zwraca Optional&lt;Task&gt;
 		Assert.assertTrue(readTask.isPresent());
 
 		//CleanUp -do "posprzątania" po testach (testy są idempotentne
 		// - czyli stan systemu po wykonaniu testu musi być taki sam jak przed wykonaniem testu
+		taskDao.deleteById(id);
+	}
+	@Test
+	public void testTaskDaoFindByDuration() {
+		//Given
+		Task task = new Task(DESCRIPTION, 7);// tworzymy obiekt task, którego wartość pola duration wynosi 7.
+		taskDao.save(task);//Zapisujemy go w bazie danych i zapamiętujemy ile wynosi duration.
+		int duration = task.getDuration();
+
+		//When
+		List<Task> readTasks = taskDao.findByDuration(duration); //wywołujemy testowaną metodę - findByDuration(int duration)
+
+		//Then
+		Assert.assertEquals(1, readTasks.size());//spr. czy zwrócony został dokładnie jeden rekord
+
+		//CleanUp
+		int id = readTasks.get(0).getId(); //sprzątamy po testach, czyli usuwamy dodany rekord
 		taskDao.deleteById(id);
 	}
 }
